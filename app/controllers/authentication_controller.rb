3a40f8b8973254b451388a9b35f155a37c5013ab
Alina Paik <alinapaik143@gmail.com>
2022-12-22 00:23:56 +0200

    start working with view

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,26 @@
+class AuthenticationController < ApplicationController
+  def login
+    user = User.find_by(email: params[:email])
+
+    if user && BCrypt::Password.new(user.password) == params[:password]
+
+      token = encode_user_data(user_id: user.id)
+      response.set_cookie(
+        :session,
+        value: token,
+        expires: 1.year.from_now,
+        secure: Rails.env.production?,
+        httponly: Rails.env.production?
+      )
+
+      render json: { message: 'login' }
+    else
+      render json: { message: 'invalid credentials' }
+    end
+  end
+
+  def logout
+    response.delete_cookie(:session)
+    render json: { message: 'log out' }
+  end
+end
